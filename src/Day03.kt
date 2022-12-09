929d657ce7520324225c5adc36b95894e4625e87
Matt <matt.friesen@streem.pro>
2022-12-09 07:27:04 -0800

    day 04 complete

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,37 +1,37 @@
 import java.io.File
 
 fun main() {
+    fun Char.toScore(): Int =
+        if (this.isUpperCase()) {
+            this - 'A' + 27
+        } else {
+            this - 'a' + 1
+        }
+
+    fun part1(input: List<String>): Long {
+        val total = input.map { rucksack ->
+            rucksack.substring(0, rucksack.length / 2) to rucksack.substring(rucksack.length / 2)
+        }
+            .flatMap { (first, second) -> first.toSet() intersect second.toSet() }
+            .sumOf { it.toScore() }.toLong()
+
+        return total
+    }
+
+    fun part2(input: List<String>): Long {
+        val total = input.chunked(3).map { elfGroup ->
+            elfGroup.zipWithNext()
+                .map { (first, second) ->
+                    first.toSet() intersect second.toSet()
+                }
+        }
+            .flatMap { sharedItems -> sharedItems[0] intersect sharedItems[1] }
+            .sumOf { it.toScore() }.toLong()
+
+        return total
+    }
+
     val input = File("src/input.txt").readLines()
     println(part1(input))
     println(part2(input))
-}
-
-private fun Char.toScore(): Int =
-    if (this.isUpperCase()) {
-        this - 'A' + 27
-    } else {
-        this - 'a' + 1
-    }
-
-fun part1(input: List<String>): Long {
-    val total = input.map { rucksack ->
-        rucksack.substring(0, rucksack.length / 2) to rucksack.substring(rucksack.length / 2)
-    }
-        .flatMap { (first, second) -> first.toSet() intersect second.toSet()}
-        .sumOf { it.toScore() }.toLong()
-
-    return total
-}
-
-fun part2(input: List<String>): Long {
-    val total = input.chunked(3).map { elfGroup ->
-        elfGroup.zipWithNext()
-            .map { (first, second) ->
-                first.toSet() intersect second.toSet()
-            }
-    }
-        .flatMap { sharedItems -> sharedItems[0] intersect sharedItems[1] }
-        .sumOf { it.toScore() }.toLong()
-
-    return total
 }
---
+++ src/Day04.kt
@@ -0,0 +1,34 @@
+import java.io.File
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        val ranges = input.map { elfAssignments ->
+            elfAssignments.asRanges()
+        }
+        return ranges.count  { it.first.fullyOverlaps(it.second) || it.second.fullyOverlaps(it.first) }
+    }
+
+    fun part2(input: List<String>): Int {
+        val ranges = input.map { elfAssignments ->
+            elfAssignments.asRanges()
+        }
+        return ranges.count  { it.first.overlaps(it.second) || it.second.overlaps(it.first) }
+    }
+
+    val input = File("src/input.txt").readLines()
+    println(part1(input))
+    println(part2(input))
+}
+
+private infix fun IntRange.fullyOverlaps(otherRange: IntRange): Boolean =
+    first <= otherRange.first && last >= otherRange.last
+
+
+private infix fun IntRange.overlaps(otherRange: IntRange): Boolean =
+    first <= otherRange.last && otherRange.first <= last
+
+private fun String.asIntRange(): IntRange =
+    substringBefore("-").toInt()..substringAfter("-").toInt()
+
+private fun String.asRanges(): Pair<IntRange, IntRange> =
+    substringBefore(",").asIntRange() to substringAfter(",").asIntRange()
